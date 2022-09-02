bb2bd425a2f9a216a5c1c0a38a763e5563075fcc
Izzet Ali <dralizzetyldrm@outlook.com>
2022-09-03 02:25:29 +0300

    çift ve 4'ün katları olan sayıları toplama

---
+++ src/KatlarToplam.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class KatlarToplam {
+    public static void main(String[] args) {
+        
+        int i,toplam=0;
+
+        Scanner input = new Scanner(System.in);
+
+        do{
+            System.out.print("Sayı Giriniz: ");
+            i=input.nextInt();
+            if((i%2==0) && (i%4==0)){
+                toplam+=i;
+            }
+        }while((i%2==0));
+        System.out.println("Toplam: " + toplam);
+    }
+}
