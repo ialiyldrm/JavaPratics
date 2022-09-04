b08eace4da71c92329dca58b63992d1d3fe00c87
Izzet Ali <dralizzetyldrm@outlook.com>
2022-09-04 17:51:58 +0300

    En büyük ve en küçük sayıyı bulma

---
+++ src/SayiBulma.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class SayiBulma {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        //Girilen sayıların en büyük ve en küçük olanlarını bulan program.
+
+        System.out.print("Kaç tane sayı gireceksiniz: ");
+        int sayi=input.nextInt();
+
+        int n=1;
+        System.out.print(n+". "+"Sayıyı giriniz: ");
+        int girdi=input.nextInt();
+        int enBuyuk=girdi;
+        int enKucuk=girdi;
+        n++;
+
+        for(int i=1;i<sayi;i++){
+            System.out.print(n+". "+"Sayıyı giriniz: ");
+            girdi=input.nextInt();
+            n++;
+            if(girdi>enBuyuk){
+                enBuyuk=girdi;
+            }
+            if(girdi<enKucuk){
+                enKucuk=girdi;
+            }
+        }
+        System.out.print("En büyük sayı:"+ enBuyuk);
+        System.out.println();
+        System.out.print("En küçük sayı:"+ enKucuk);
+        
+    }
+    
+}
+
