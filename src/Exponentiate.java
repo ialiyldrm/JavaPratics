618807a85645e34b7771f4215e7890bb42bf5768
Izzet Ali <dralizzetyldrm@outlook.com>
2022-09-07 01:50:14 +0300

    Negatif değerlerle üs alma eklendi.

--- src/Exponentiate.java
+++ src/Exponentiate.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Exponentiate {
     public static void main(String[] args) {
         //Java ile kullanıcının girdiği değerler ile üslü sayı hesaplayan program
-        int a,b,total=1;
+        int a,b;
         Scanner input=new Scanner(System.in);
 
         System.out.print("Üssü alınacak sayıyı giriniz:");
@@ -11,9 +11,19 @@ public class Exponentiate {
         System.out.print("Üs olacak sayıyı giriniz:");
         b=input.nextInt();
 
-        for(int i=1 ; i<=b ; i++){
-            total*=a;     
+        double total=1;
+
+        if(b>0){
+            for(int i=1 ; i<=b ; i++){
+                total*=a;     
+            }
         }
+        else if(b<0){
+            for(int i=-1;i>=b;i--){
+                total*=a;
+            }
+            total=(1/total);
+        }  
         System.out.print(a+"^"+b+"="+total);
     }
     
