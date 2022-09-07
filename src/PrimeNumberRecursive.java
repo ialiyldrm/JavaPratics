7cbe4c18253f6dd58068313d7eb8f22b168708f4
Izzet Ali <dralizzetyldrm@outlook.com>
2022-09-07 13:23:29 +0300

    Loop düzenlendi

--- src/PrimeNumberRecursive.java
+++ src/PrimeNumberRecursive.java
@@ -5,26 +5,31 @@ public class PrimeNumberRecursive {
     static void primeNumber(int number){
 
         boolean result=false;
-        for(int i=2;i<number;i++){
-            if(number%i==0){
-                result=false;
-                break;
-            }else{
-                result=true;
-                break;
+        if(number==2){
+            result=true;
+        }else{
+            for(int i=2;i<number;i++){
+                if(number%i==0){
+                    result=false;
+                    break;
+                
+                }else{
+                    result=true;                    
+                }
             }
         }
+        
         if(result){
-            System.out.println(number + " ASALDIR!");
+            System.out.println(number + " SAYISI ASALDIR!");
         }else{
-            System.out.println(number + " ASAL DEĞİLDİR!");
+            System.out.println(number + " SAYISI ASAL DEĞİlDİR!");
         }
     
     }    
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
 
-        System.out.println("Sayı Giriniz:");
+        System.out.print("Sayı Giriniz:");
         int number=input.nextInt();
 
         primeNumber(number);
